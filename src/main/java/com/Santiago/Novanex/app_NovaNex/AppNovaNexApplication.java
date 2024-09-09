3f670af069440907b944eced635c7736a3682846
SantiTech-HM <shoyo01@misena.edu.co>
2024-09-08 19:05:18 -0500

    proyecto base

---
+++ src/main/java/com/Santiago/Novanex/app_NovaNex/AppNovaNexApplication.java
@@ -0,0 +1,13 @@
+package com.Santiago.Novanex.app_NovaNex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNovaNexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppNovaNexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Santiago/Novanex/app_NovaNex/AppNovaNexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Santiago.Novanex.app_NovaNex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNovaNexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
