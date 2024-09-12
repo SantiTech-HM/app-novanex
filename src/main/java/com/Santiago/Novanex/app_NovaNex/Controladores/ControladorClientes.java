95d3a3eee04b9c52c60696575b47500e9be2153c
SantiTech-HM <shoyo01@misena.edu.co>
2024-09-12 00:13:41 -0500

    CRUD completo para tabla clientes

--- src/main/java/com/Santiago/Novanex/app_NovaNex/Controladores/ControladorClientes.java
+++ src/main/java/com/Santiago/Novanex/app_NovaNex/Controladores/ControladorClientes.java
@@ -12,19 +12,19 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/")
 @CrossOrigin(value = "http://localhost:4200")
 public class ControladorClientes {
 
     @Autowired
     private IServicioClientes iServicioClientes;
 
-    @GetMapping ("/clientes")
+    @GetMapping ("clientes")
     public List<Clientes> getClientes() {
         return iServicioClientes.listClientes();
     }
 
-    @GetMapping ("/cliente/{id}")
+    @GetMapping ("cliente/{id}")
     public ResponseEntity<Clientes> getClienteById(@PathVariable Integer id) {
        Clientes clientes = iServicioClientes.findClientesById(id);
        if (clientes == null){
@@ -32,22 +32,28 @@ public class ControladorClientes {
        }
        return ResponseEntity.ok(clientes);
     }
-    @PostMapping ("/clientes")
+    @PostMapping ("clientes")
     public Clientes postCliente(@RequestBody Clientes clientes) {
         return iServicioClientes.saveClientes(clientes);
     }
-    @PutMapping ("/clientes/{id}")
+    @PutMapping ("clientes/{id}")
     public ResponseEntity<Clientes> putClienteById(@PathVariable Integer id, @RequestBody Clientes clientesObjeto) {
         Clientes clientes = iServicioClientes.findClientesById(id);
         if (clientes == null){
             throw new ExceptionNoFound("ID de cliente no encontrado:" + id);
         }
-        Clientes.setNameClientes(clientesObjeto.getNameClientes());
+        clientes.setNombre(clientesObjeto.getNombre());
+        clientes.setApellidos(clientesObjeto.getApellidos());
+        clientes.setCorreo(clientesObjeto.getCorreo());
+        clientes.setTelefono(clientesObjeto.getTelefono());
+        clientes.setDireccion(clientesObjeto.getDireccion());
+        clientes.setFechaNacimiento(clientesObjeto.getFechaNacimiento());
+
 
         Clientes updateClientes = iServicioClientes.saveClientes(clientes);
         return ResponseEntity.ok(updateClientes);
     }
-    @DeleteMapping("/clientes/{id}")
+    @DeleteMapping("clientes/{id}")
     public ResponseEntity<Map<String, Boolean>> deleteClientesById(@PathVariable Integer id){
         Clientes clientes = iServicioClientes.findClientesById(id);
         if (clientes == null){
--- src/main/java/com/Santiago/Novanex/app_NovaNex/EntidadesOModelos/Clientes.java
+++ src/main/java/com/Santiago/Novanex/app_NovaNex/EntidadesOModelos/Clientes.java
@@ -2,10 +2,7 @@ package com.Santiago.Novanex.app_NovaNex.EntidadesOModelos;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 
 import java.time.LocalDate;
@@ -15,17 +12,77 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Table(name = "clientes")
 public class Clientes {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
+
+
+    @Column(name = "nombre")
     private String nombre;
+
+    @Column(name = "apellidos")
     private String apellidos;
+
+    @Column(name = "correo")
     private String correo;
+
+    @Column(name = "telefono")
     private String telefono;
+
+    @Column(name = "direccion")
     private String direccion;
+
+    @Column(name = "fecha_nacimiento")
     private LocalDate fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
